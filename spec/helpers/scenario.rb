e769d2217c1a33dcff687eecac1cb378f7889a2d
Jeremiah A. Bohling <jeremiah.bohling@gmail.com>
2023-01-03 19:42:04 -0600

    Create Scenario spec helper to generate game turns

---
+++ spec/helpers/scenario.rb
@@ -0,0 +1,103 @@
+class Scenario
+  class << self
+    def generate(scenario, health: {})
+      rows = scenario.split("\n").reverse
+
+      snakes = {}
+      food = []
+
+      uppers = ("A".."Z")
+      lowers = ("a".."z")
+
+      rows.each_with_index do |row, y|
+        row.chars.each_with_index do |char, x|
+          location = {"x" => x, "y" => y}
+
+          case char
+          when "*"
+            food << location
+          when uppers
+            snakes[char] ||= []
+            snakes[char].prepend(location)
+          when lowers
+            key = char.upcase
+            snakes[key] ||= []
+            snakes[key] << location
+          end
+        end
+      end
+
+      {
+        "game" => {
+          "id" => "totally-unique-game-id",
+          "ruleset" => {
+            "name" => "standard",
+            "version" => "v1.1.15",
+            "settings" => {
+              "foodSpawnChance" => 15,
+              "minimumFood" => 1,
+              "hazardDamagePerTurn" => 14
+            }
+          },
+          "map" => "standard",
+          "source" => "league",
+          "timeout" => 500
+        },
+        "turn" => 14,
+        "board" => generate_board(snakes: snakes.sort.to_h, health: health, food: food)
+      }.tap do |data|
+        data["you"] = data["board"]["snakes"].first
+      end
+    end
+
+    private
+
+    def generate_board(snakes: nil, health: {}, food: nil)
+      food ||= [
+        {"x" => 5, "y" => 5},
+        {"x" => 9, "y" => 0},
+        {"x" => 2, "y" => 6}
+      ]
+
+      {
+        "height" => 11,
+        "width" => 11,
+        "food" => food,
+        "hazards" => [],
+        "snakes" => []
+      }.tap do |board|
+        snakes ||= {
+          "A" => [
+            {"x" => 0, "y" => 0},
+            {"x" => 1, "y" => 0},
+            {"x" => 2, "y" => 0}
+          ],
+          "B" => [
+            {"x" => 5, "y" => 4},
+            {"x" => 5, "y" => 3},
+            {"x" => 6, "y" => 3},
+            {"x" => 6, "y" => 2}
+          ]
+        }
+
+        snakes.each do |name, body|
+          board["snakes"] << {
+            "id" => "snake-508e96a#{name.downcase}-94ad-11ea-bb37",
+            "name" => "Snake #{name}",
+            "health" => health[name] || 50,
+            "body" => body,
+            "latency" => "123",
+            "head" => body.first,
+            "length" => body.size,
+            "shout" => "",
+            "customizations" => {
+              "color" => "#000000",
+              "head" => "default",
+              "tail" => "default"
+            }
+          }
+        end
+      end
+    end
+  end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,101 +1,25 @@
 require "./lib/player"
+require "./spec/helpers/scenario"
 require "json"
 
 RSpec.describe Player do
   describe "::move" do
+    let(:health) { 51 }
+
     let(:data) do
-      JSON.parse(<<~JSON)
-        {
-          "game": {
-            "id": "totally-unique-game-id",
-            "ruleset": {
-              "name": "standard",
-              "version": "v1.1.15",
-              "settings": {
-                "foodSpawnChance": 15,
-                "minimumFood": 1,
-                "hazardDamagePerTurn": 14
-              }
-            },
-            "map": "standard",
-            "source": "league",
-            "timeout": 500
-          },
-          "turn": 14,
-          "board": {
-            "height": 11,
-            "width": 11,
-            "food": [
-              {"x": 5, "y": 5},
-              {"x": 9, "y": 0},
-              {"x": 2, "y": 6}
-            ],
-            "hazards": [
-              {"x": 3, "y": 2}
-            ],
-            "snakes": [
-              {
-                "id": "snake-508e96ac-94ad-11ea-bb37",
-                "name": "My Snake",
-                "health": 54,
-                "body": [
-                  {"x": 0, "y": 0},
-                  {"x": 1, "y": 0},
-                  {"x": 2, "y": 0}
-                ],
-                "latency": "111",
-                "head": {"x": 0, "y": 0},
-                "length": 3,
-                "shout": "why are we shouting??",
-                "customizations":{
-                  "color":"#FF0000",
-                  "head":"pixel",
-                  "tail":"pixel"
-                }
-              },
-              {
-                "id": "snake-b67f4906-94ae-11ea-bb37",
-                "name": "Another Snake",
-                "health": 16,
-                "body": [
-                  {"x": 5, "y": 4},
-                  {"x": 5, "y": 3},
-                  {"x": 6, "y": 3},
-                  {"x": 6, "y": 2}
-                ],
-                "latency": "222",
-                "head": {"x": 5, "y": 4},
-                "length": 4,
-                "shout": "I'm not really sure...",
-                "customizations":{
-                  "color":"#26CF04",
-                  "head":"silly",
-                  "tail":"curled"
-                }
-              }
-            ]
-          },
-          "you": {
-            "id": "snake-508e96ac-94ad-11ea-bb37",
-            "name": "My Snake",
-            "health": 54,
-            "body": [
-              {"x": 0, "y": 0},
-              {"x": 1, "y": 0},
-              {"x": 2, "y": 0}
-            ],
-            "latency": "111",
-            "head": {"x": 0, "y": 0},
-            "length": 3,
-            "shout": "why are we shouting??",
-            "customizations": {
-              "color":"#FF0000",
-              "head":"pixel",
-              "tail":"pixel"
-            }
-          }
-        }
-      JSON
+      Scenario.generate(<<~TXT, health: {"A" => health})
+        ...........
+        ...........
+        ...........
+        ...........
+        ..*........
+        .....*.....
+        .....B.....
+        .....bb....
+        ......b....
+        Aaa........
+        .....*.....
+      TXT
     end
 
     subject { Player.move(data) }
@@ -103,11 +27,9 @@ RSpec.describe Player do
     it { is_expected.to eq("up") }
 
     describe "with low health" do
-      before do
-        data["you"]["health"] = 50
-      end
+      let(:health) { 50 }
 
-      it { is_expected.to eq("up") }
+      it { is_expected.to eq("down") }
     end
   end
 end
